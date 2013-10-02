cdfa670ff41aeae997d8f2a04cee99aa18d86ee6
Sean McArthur <sean.monstar@gmail.com>
2013-10-02 11:02:39 -0700

    starting some benchmarking!
    simple benchmark of console.log, intel.info, and winston.info

---
+++ benchmark/logging.js
@@ -0,0 +1,40 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+const Console = require('console').Console;
+const EE = require('events').EventEmitter;
+
+const winston = require('winston');
+const intel = require('../');
+
+var stdout = new EE();
+stdout.write = function (out, encoding, cb) {
+  if (typeof encoding === 'function') {
+    cb = encoding;
+    encoding = null;
+  }
+  cb && cb();
+  return true;
+};
+
+var _console = new Console(stdout, stdout);
+intel.addHandler(new intel.handlers.Stream(stdout));
+
+winston.add(winston.transports.File, { stream: stdout });
+winston.remove(winston.transports.Console);
+
+module.exports = {
+  'logging': {
+    bench: {
+      'console.info': function() {
+        _console.info('asdf');
+      },
+      'intel.info': function() {
+        intel.info('asdf');
+      },
+      'winston.info': function() {
+        winston.info('asdf');
+      }
+    }
+  }
+};
