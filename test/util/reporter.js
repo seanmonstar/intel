60ea551222d8bfa2828a645ec09f31770e02987d
Sean McArthur <sean.monstar@gmail.com>
2017-06-01 09:50:45 -0700

    update dependencies, use nyc instead of blanket

--- test/util/reporter.js
+++
@@ -1,9 +0,0 @@
-var cov = require('mocha-text-cov');
-var dots = require('mocha/lib/reporters/dot');
-
-function intelReporter(runner, opts) {
-  this._dots = new dots(runner, opts);
-  this._cov = new cov(runner, opts);
-}
-
-module.exports = intelReporter;
